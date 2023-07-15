e95f7505d07f6682da2bd67e65f935d87b3e08b8
Adheeban Manoharan <adheeban.m@saama.com>
2023-07-15 18:17:56 +0530

    Adding first iteration

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import NavBar from "./NavBar";
+import GraphMod from "./Body";
+
+
+function App() {
+  return (
+    <div className="bg-gradient-to-tl from-emerald-400 via-teal-200 to-cyan-100 p-4">
+      <NavBar/>   
+      <GraphMod/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Body.js
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import ChartComponent from "./Chart";
+
+const tickList = ["AAPL", "TATAMOTORS", "HCLINFO", "TATAELXSI"];
+
+function TickerComponent() {
+  const [ticker, setTicker] = useState("");
+
+  function changeTicker(e) {
+    var newTicker = e.target.innerText;
+    setTicker("");
+    document.getElementById("ticker").value = newTicker;
+  }
+
+  function searchTicker() {
+    var filList = tickList.filter((tick) =>
+      tick.startsWith(ticker.toUpperCase())
+    );
+    return (
+      <div className="absolute p-2 gap-2 flex-col rounded-lg bg-white shadow-lg divide-gray-50 text-gray-600 mx-2 z-40">
+        {filList.map((tick) => (
+          <div
+            className="p-2 hover:bg-gray-300"
+            onClick={(e) => changeTicker(e)}
+          >
+            {tick}
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <div className="relative">
+      <label
+        for="ticker"
+        className="block mb-2 text-sm font-medium text-emerald-800"
+      >
+        Ticker
+      </label>
+      <input
+        placeholder="TCS"
+        type="text"
+        id="ticker"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-emerald-500 focus:border-emerald-800 block w-full p-2.5"
+        onChange={(e) => setTicker(e.target.value)}
+      />
+      {ticker.length > 0 ? searchTicker() : <div />}
+    </div>
+  );
+}
+
+function CountryComponent() {
+  return (
+    <div className="relative">
+      <label
+        for="country"
+        className="block mb-2 text-sm font-medium text-emerald-800"
+      >
+        Select a market
+      </label>
+      <select
+        id="country"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-emerald-500 focus:border-emerald-800 block w-full p-2.5"
+      >
+        <option selected value="IN">
+          India
+        </option>
+        <option value="US">United States</option>
+      </select>
+    </div>
+  );
+}
+
+function TimeRangeComponent() {
+    return (
+      <div className="relative">
+        <label
+          for="range"
+          className="block mb-2 text-sm font-medium text-emerald-800"
+        >
+          Select range
+        </label>
+        <select
+          id="range"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-emerald-500 focus:border-emerald-800 block w-full p-2.5"
+        >
+          <option selected value="1M">1M</option>
+          <option value="3M">3M</option>
+          <option value="6M">6M</option>
+          <option value="1Y">1Y</option>
+        </select>
+      </div>
+    );
+  }
+
+export default function GraphMod() {
+  return (
+    <div className="space-y-5">
+      <div className="flex items-stretch bg-white rounded-xl mx-auto p-4 flex-col justify-between space-x-2 space-y-5 w-1/3 shadow-xl">
+        <CountryComponent />
+        <TickerComponent />
+      </div>
+      <div className="flex items-center bg-white rounded-xl mx-auto p-4 flex-col justify-between space-x-2 space-y-10 w-1/3 shadow-xl">
+        <ChartComponent />
+        <TimeRangeComponent/>
+      </div>
+    </div>
+  );
+}
